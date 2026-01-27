4cab79b011cf0bcbcd11a41bf285473efe14441f
tatui1 <kamila.lumazova@compasscollege.art>
2026-01-27 20:28:36 +0600

    tying props

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { AddTodo } from "../AddTodo/AddTodo";
-import { TodoList } from "../TodoList/TodoList";
+import { useState } from "react"
+import { AddTodo } from "../AddTodo/AddTodo"
+import { TodoList } from "../TodoList/TodoList"
 
 
 export const Todo = () => {
-
+    
     const [todos, setTodos] = useState([])
 
     const onAddTodo = (newTodoString) => {
@@ -15,10 +15,14 @@ export const Todo = () => {
         setTodos([...todos, newTodo])
     }
 
-    return(
-        <div>
+    const onDeleteTodo = (id) => {
+        setTodos(todos.filter(item => item.id !==id))
+    }
+    return (
+        <div className="todo-container">
+            <h1>Your watch list:</h1>
             <AddTodo onAddTodo={onAddTodo}/>
-            <TodoList todos={todos}/>  
+            <TodoList todos={todos} onDeleteTodo={onDeleteTodo}/>  
         </div>
     )
 }
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -5,9 +5,9 @@ export const TodoItem = (props) => {
     return (
     <p>
         {props.todo.title}
+      <input type="text" value={props.todo.title} readOnly/>
       <button onClick={() => props.onDeleteTodo(props.todo.id)}>delete</button>
     </p>
   )
 
 }
-
